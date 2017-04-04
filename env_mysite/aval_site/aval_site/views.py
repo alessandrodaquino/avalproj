7fc6e50b6c1263b1e94bd32a0df0238fc24de5dc
Alessandro Daquino <alessandro.daquino@controller.education>
2017-04-04 01:27:11 -0300

    salvando candidato e validando se já existe na base ou não. - passando o id do usuário por parametro da primeira para a segunda tela - lista de perguntas vem da base de dados - lista de respostas ja é enviado para o servidor porem não é registrado

--- env_mysite/aval_site/aval_site/views.py
+++ env_mysite/aval_site/aval_site/views.py
@@ -1,6 +1,8 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.db import connection
+from aval.models import Candidates
 
 def start(request):
     """start funcion, for main/welcome page"""
@@ -11,14 +13,44 @@ def is_valid_string(s):
         s = s.strip()
         if len(s) > 0:
             return True
+
     return False
+
+def create_candidate(db, post_data):
+    cand = Candidates(name=post_data['name'], email=post_data['email'])
+    cand.save()
+    return cand.pk
+
+def login_page_post(request, post_data):
+    db = connection.cursor()
+    db.execute("SELECT 1 FROM AVAL_CANDIDATES WHERE EMAIL = %s", [post_data['email']])
+    row = db.fetchone()
+
+    if row == None:
+        id = create_candidate(db, post_data)
+        return JsonResponse({
+            'status': 'ok',
+            'idcandidate': id
+        })
+    else:
+        return JsonResponse({
+            'status': 'cand_exists',
+            'msg': 'Candidato já cadastrado. Obrigado pelo interesse.'
+        })
+
+    return JsonResponse({'status': 'err', 'msg': 'Erro desconhecido'})
 
 def checklogin(request, post_data):
     err = False
+    fieldname_valid = False
+    fieldemail_valid = False
+
     if 'name' in post_data:
         is_valid_name = is_valid_string(post_data['name'])
         if not is_valid_name:
             err = True
+        else:
+            fieldname_valid = True
     else:
         err = True
 
@@ -26,13 +58,51 @@ def checklogin(request, post_data):
         is_valid_mail = is_valid_string(post_data['email'])
         if not is_valid_mail:
             err = True
+        else:
+            fieldemail_valid = True
     else:
         err = True
 
     if err:
-        msg = 'Nome ou e-mail inválido'
-        return JsonResponse({'status': 'err', 'msg': msg})
+        msg = ''
+        if (not fieldemail_valid) and (not fieldname_valid):
+            msg = 'Nome e e-mail inválidos'
+        elif not fieldemail_valid:
+            msg = 'E-mail inválido'
+        elif not fieldname_valid:
+            msg = 'Nome inválido'
 
+        return JsonResponse({
+            'status': 'err',
+            'msg': msg,
+            'fname': fieldname_valid,
+            'femail': fieldemail_valid
+        })
+    else: #se nenhum erro for encontrado, prossegue para a base de dados
+        return login_page_post(request, post_data)
+
+    return JsonResponse({'status': 'ok'})
+
+def load_quest_page(request, get_data):
+    db = connection.cursor()
+    db.execute("SELECT id, description FROM aval_questions")
+    fieldnames = [name[0] for name in db.description]
+    result = []
+    for row in db.fetchall():
+        rowset = []
+        for field in zip(fieldnames, row):
+            rowset.append(field)
+        result.append(dict(rowset))
+    return JsonResponse({'status': 'ok', 'questions': json.dumps(result)})
+
+def save_answers(request, post_data):
+    answers = post_data['answers']
+    print(answers)
+    for answer in answers:
+        for k, v in answer.items():
+            print(k, v)
+
+    #     print(answers[item])
     return JsonResponse({'status': 'ok'})
 
 def fix_post_data(request):
@@ -52,12 +122,17 @@ def fix_get_data(request):
 
 def route(request):
     if request.method == 'GET':
-        pass
-        # get_data = fix_get_data(request)
-        # if get_data['page'] == 'questionario':
-        #     return render(request, 'quest.html', {})
+        get_data = fix_get_data(request)
+
+        if get_data['page'] == 'quest':
+            return load_quest_page(request, get_data)
+
     elif request.method == 'POST':
         post_data = fix_post_data(request)
+
         if post_data['page'] == 'login':
             return checklogin(request, post_data)
+        elif post_data['page'] == 'quest':
+            return save_answers(request, post_data)
+
     return render(request, 'base.html', {})
--- env_mysite/aval_site/static/scripts/js/controllers/aval.js
+++ env_mysite/aval_site/static/scripts/js/controllers/aval.js
@@ -9,15 +9,23 @@ var url = '/aval/'
 function LoginCtrl( $scope, $http, $location ) {
     $scope.login = {};
     $scope.enablebtn = true;
-    
+    $scope.valid_input = {}
+    $scope.valid_input.name = true; //Input for name is valid or not
+    $scope.valid_input.email = true;
+    $scope.err_msg = ''
+
     $scope.senddata = function(){
         $scope.login.page = 'login'
         $http.post(url, $scope.login)
             .then(function(response){
                 if (response.data.status == 'ok'){
-                    $location.path('/questionario/')
+                    console.log(response.data)
+                    $location.path('/questionario/'+ response.data.idcandidate)
                 }else{
-                    console.log(response.data.msg)
+                    console.log(response.data)
+                    $scope.err_msg = response.data.msg;
+                    $scope.valid_input.name = response.data.fname;
+                    $scope.valid_input.femail = response.data.femail;
                 }   
             }
         );
@@ -25,45 +33,60 @@ function LoginCtrl( $scope, $http, $location ) {
 }
 LoginCtrl.$inject = ['$scope', '$http', '$location'];
 
-function QuestController($scope, $http, $location){
-    console.log('questionários baby...')
+function QuestController($scope, $http, $location, $routeParams){
+    $scope.answer = {};
+    $scope.qtn_list = [];
+    var idcandidate = $routeParams.idcandidate;
+
+    $scope.opts = [
+        {'val': 0, 'desc': 0},
+        {'val': 1, 'desc': 1},
+        {'val': 2, 'desc': 2},
+        {'val': 3, 'desc': 3},
+        {'val': 4, 'desc': 4},
+        {'val': 5, 'desc': 5},
+        {'val': 6, 'desc': 6},
+        {'val': 7, 'desc': 7},
+        {'val': 8, 'desc': 8},
+        {'val': 9, 'desc': 9},
+        {'val': 10, 'desc': 10},
+    ]
+
+    $scope.senddata = function(){
+        $scope.answer.page = 'quest'
+        $scope.answer.answers = $scope.qtn_list
+        $http.post(url, $scope.answer)
+            .then(function(response){
+                if (response.data.status == 'ok'){
+                    
+                }
+            });
+    };
+
+    $scope.onload = function(){
+        console.log(idcandidate)
+        $http.get(url + '?page=quest')
+            .then(function(response){
+                if (response.data.status == 'ok'){
+                    $scope.qtn_list = JSON.parse(response.data.questions)
+                }
+            }
+        );
+    };
+
+    $scope.onload()
+
+
 }
-QuestController.$inject = ['$scope', '$http', '$location'];
+QuestController.$inject = ['$scope', '$http', '$location', '$routeParams'];
 
 function aval_config( $routeProvider ) {
     var pfx = '/static/templates/';
 
     $routeProvider
-        .when( '/',                            { templateUrl: pfx + 'main.html'            })
-        .when( '/questionario/',         { templateUrl: pfx + 'quest.html'         })
+        .when( '/', { templateUrl: pfx + 'main.html'})
+        .when( '/questionario/:idcandidate', { templateUrl: pfx + 'quest.html'})
         .otherwise({redirectTo: '/' })
         ;
 }
 aval_config.$inject = [ '$routeProvider' ];
-
-// angular.module('avalApp', [])
-//     .controller('MainController', function($http) {
-//     var mainCtlr = this;
-//     var url = '/candidate/'
-//     var candidate_id = null
-
-//     mainCtlr.magic = function(){
-//         alert('nice')
-//         console.log('nice')
-//     };
-
-//     mainCtlr.senddata = function(){
-//         //TODO - criar validação dos campos de nome e email
-//         var post_data = {}
-//         post_data.page = 'login'
-//         post_data.name = mainCtlr.name;
-//         post_data.email = mainCtlr.email;
-//         $http.post(url, post_data).then(function(response){
-//                 var data = response.data
-//                 if (response.status == 200 && data.status == 'ok') {
-//                     $http.get(url+'?page=questionario')
-//                 }
-//             });
-//     };
-
-// });
